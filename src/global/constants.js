900bd7d7799a3b8e4ccaa60724cd8defbbd706db
Invincible <prathameshsujgure33@gmail.com>
2022-04-18 02:27:38 +0530

    Logic done, ui features remaining, temporary ui done

--- src/global/constants.js
+++ src/global/constants.js
@@ -131,4 +131,20 @@ const NameKeysClass = {
   ControlRight: "lilF",
   Arrow: "lilF",
 };
-export { NameKeys0, NameKeys1, NameKeys2, NameKeys3, NameKeys4, NameKeysClass };
+const InputDisplayTexts = [
+  "It is a curious thing, Harry, but perhaps those who are best suited to power are those who have never sought it. Those who, like you, have leadership thrust upon them, and take up the mantle because they must, and find to their own surprise that they wear it well",
+  "Spoken like a true leader. But since your failure as a leader is a virtual certainty, tolerating your short reign as CEO in exchange for a front-row seat to the disaster seems fair. Plus, if I'm wrong, which I'm not, I get rich. So I'm down with it, Dinesh.",
+  "Tyrion Lannister recognizes Jon Snow as the illegitimate son of Ned Stark and calls him a bastard. This does offend Jon Snow to a great degree. This is when Lannister gives him some advice… Never forget what you are. The rest of the world will not. Wear it like armour, and it can never be used to hurt you.",
+  "The hole in one's heart gets filled by others around you. Friends won't flock to someone who abandons the memory of his friends and gives up on the world just because things don't go the way he wants them to. And people won't help those who run away and do nothing. As long as you don't give up, there will always be salvation.",
+  "Iron Man achieved something rare for a superhero. They made a hero who was just as interesting outside of the costume as he was inside the costume. Instead of playing the secret identity game, Tony Stark and Iron Man are one and the same and this becomes the mantra for the character.",
+  "There is a kind of fallout that happens when you leave college. Your professor gets paid to pay attention to your ideas, and your classmates are paying to pay attention to your ideas. Never again in your life will you have such a captive audience. It’s not that people are mean or cruel, they’re just busy.",
+];
+export {
+  NameKeys0,
+  NameKeys1,
+  NameKeys2,
+  NameKeys3,
+  NameKeys4,
+  NameKeysClass,
+  InputDisplayTexts,
+};
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./MainPage.css";
 import NavBar from "../components/NavBar";
 import Keyboard from "../components/Keyboard";
